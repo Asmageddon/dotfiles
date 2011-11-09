5362b1668e0c1ffb6b0f83a8fe7ae8fa3b56ec08
Asmageddon <Asmageddon@gmail.com>
2011-11-09 23:00:05 +0100

    Updated install.sh script

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ for file in $shell_files ; do
 	eval $COMMAND
 done
 
-other_files="/tmux.conf .conkyrc"
+other_files=".tmux.conf .conkyrc"
 for file in $other_files ; do
 	COMMAND="ln -s $PWD/$file $installation_dir/$file"
 	echo "Creating a symlink from $file to $installation_dir/$file"
