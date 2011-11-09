a2f0613e4d0c27f35191e8f53d6a645061a4abdf
Asmageddon <Asmageddon@gmail.com>
2011-11-09 23:39:57 +0100

    Minor changes

--- install.sh
+++ install.sh
@@ -9,14 +9,14 @@ fi
 shell_config_dir="shell"
 shell_files=".aliases .bashrc .promptrc .shvars .zshrc .bash_profile .functions .shrc .termtitle"
 for file in $shell_files ; do
+	chmod +x "$shell_config_dir/$file"
 	COMMAND="ln -s $PWD/$shell_config_dir/$file $installation_dir/$file"
-	echo "Creating a symlink from $shell_config_dir/$file to $installation_dir/$file"
 	eval $COMMAND
 done
 
 other_files=".tmux.conf .conkyrc"
 for file in $other_files ; do
+	chmod +x $file
 	COMMAND="ln -s $PWD/$file $installation_dir/$file"
-	echo "Creating a symlink from $file to $installation_dir/$file"
 	eval $COMMAND
 done
